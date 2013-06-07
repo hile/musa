c3eacc7dbeb97ed75c9d65b6722f1fde95df21ab
Ilkka Tuohela <hile@iki.fi>
2013-06-07 04:29:07 +0300

    Fix codec unit test to new API

--- test/test_codecs.py
+++ test/test_codecs.py
@@ -1,27 +1,31 @@
 
 import os,re
 import unittest
-from musa.formats import CODECS,MusaFileFormat,filter_available_command_list,match_codec
+
+from musa.models import Codec
+from musa.config import MusaConfigDB
+from musa.formats import MusaFileFormat,filter_available_command_list,match_codec
+
 
 class test_codecs(unittest.TestCase):
-    def test_codec_config_type(self):
-        self.assertIsInstance(CODECS,dict)
-
     def test_codec_config_keys(self):
-        for k in CODECS.keys():
-            self.assertEquals(k,match_codec(k))
+        db = MusaConfigDB()
+        for k in db.codecs.keys():
+            self.assertIsInstance(match_codec(k),Codec)
+            self.assertEquals(k,match_codec(k).name)
 
-        for k,v in CODECS.items():
-            if 'extensions' not in v:
+        for k,v in db.codecs.items():
+            if 'extensions' not in v.extensions:
                 continue
             for ext in v['extensions']:
-                self.assertEquals(k,match_codec(ext))
+                self.assertEquals(k,match_codec(ext).name)
 
     def test_fileformat_loader(self):
-        for k in CODECS.keys():
+        db = MusaConfigDB()
+        for k in db.codecs.keys():
             testpath = os.path.join('/tmp/test.%s' % k)
             musaformat = MusaFileFormat(testpath)
-            self.assertEquals(musaformat.codec,k)
+            self.assertEquals(k,musaformat.codec.name)
             self.assertIsInstance(musaformat.get_available_encoders(),list)
             self.assertIsInstance(musaformat.get_available_decoders(),list)
 
