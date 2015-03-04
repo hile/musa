b74c482bd04450d28e7821614ecd5697260617e9
Ilkka Tuohela <ilkka.tuohela@codento.com>
2015-03-04 09:19:23 +0200

    Point package URL to github

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-import os,glob
+import glob
 from setuptools import setup, find_packages
 
-VERSION='3.4.1'
+VERSION='3.4.2'
 
 setup(
     name = 'musa',
@@ -13,10 +13,10 @@ setup(
     author_email = 'hile@iki.fi',
     description = 'Module for music tagging and library management',
     keywords = 'music library tag management',
-    url = 'http://tuohela.net/packages/musa',
-    packages = ( 'musa', ),
+    url = 'https://github.com/hile/musa',
     package_data = { '': [ '*.md', '*.txt' ] },
     scripts = glob.glob('bin/*'),
+    packages = find_packages(),
     install_requires = ( 
         'configobj', 
         'soundforest>=3.4.4', 
